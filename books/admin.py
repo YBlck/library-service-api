33d14590d56c763ccbb238f84cf81b9ce704bace
Yura Chornyi <chorny.yura@gmail.com>
2025-04-11 16:05:06 +0300

    feat: register Book to admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from books.models import Book
+
+admin.site.unregister(Group)
+
+admin.site.register(Book)
