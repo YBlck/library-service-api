7c4f7d3ccf3764e98cab98b1e755461e7b7737fd
Yura Chornyi <chorny.yura@gmail.com>
2025-04-12 04:42:08 +0300

    feat: add base Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrowing_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowings"
+    )
+
+    def __str__(self):
+        return f"{self.borrowing_date}-{self.book}-{self.user}-{self.expected_return_date}"
