f5faf1d90595ce8351457170af03cabdd91827b1
Yura Chornyi <chorny.yura@gmail.com>
2025-04-13 02:04:19 +0300

    feat: add method reduce_inventory to Book model
    - This method reduces inventory of book by 1

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Book(models.Model):
@@ -19,3 +20,9 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} (author: {self.author})"
+
+    def reduce_inventory(self):
+        if self.inventory > 0:
+            Book.objects.filter(pk=self.pk).update(inventory=self.inventory - 1)
+            self.refresh_from_db()
+        raise ValidationError("This book is out of stock")
