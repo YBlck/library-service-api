edc75ad7fa9cbba1d1b41b4e4a823e8a70424b1b
Yura Chornyi <chorny.yura@gmail.com>
2025-04-14 16:34:12 +0300

    feat: add logging for bot

--- notifications/bot.py
+++ notifications/bot.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 
 from dotenv import load_dotenv
@@ -10,6 +11,15 @@ CHAT_ID = getenv("CHAT_ID")
 
 bot = Bot(token=TOKEN)
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
 
 def send_notification(message: str):
-    bot.send_message(chat_id=CHAT_ID, text=message)
+    try:
+        bot.send_message(chat_id=CHAT_ID, text=message)
+        logging.info("Message sent to Telegram")
+    except Exception as e:
+        logging.error(f"Failed to send Telegram message: {e}")
