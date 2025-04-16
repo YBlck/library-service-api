9e274c0081789cda112ff9dde150977b8d3b9315
Yura Chornyi <chorny.yura@gmail.com>
2025-04-16 23:15:38 +0300

    feat: add list and detail endpoints
    - admin can see all payments
    - user can see only its own payments

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentsViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = self.queryset
+        if self.action in ["retrieve", "list"]:
+            if user.is_staff:
+                return queryset
+            return queryset.filter(borrowing__user__id=user.id)
+
+        return queryset
